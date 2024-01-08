03b3368de2a57a5093882325259e09dedd2149a5
sakibmohammad79 <sakibmohammad7679@gmail.com>
2024-01-08 19:45:15 +0600

    reducer function concept done

--- src/components/UserInfoWithUseReducer.tsx
+++ src/components/UserInfoWithUseReducer.tsx
@@ -1,15 +1,27 @@
-import { useReducer } from "react";
+import { ChangeEvent, useReducer } from "react";
 
 const initialState = {
   name: "",
   age: "",
-  hobbies: [],
+  hobbies: [] as string[],
 };
 
-const reducer = (currentState, action) => {
+type TAction = {
+  type: string;
+  payload: string;
+};
+
+const reducer = (currentState: typeof initialState, action: TAction) => {
   switch (action.type) {
     case "addName":
-      return { ...currentState, name: "Sakib" };
+      return { ...currentState, name: action.payload };
+    case "addAge":
+      return { ...currentState, age: action.payload };
+    case "addHobby":
+      return {
+        ...currentState,
+        hobbies: [...currentState.hobbies, action.payload],
+      };
 
     default:
       return initialState;
@@ -19,10 +31,16 @@ const reducer = (currentState, action) => {
 const UserInfoWithUseReducer = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
   console.log(state);
+
+  const handleSubmit = (e: ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(state);
+  };
+
   return (
-    <form className="">
+    <form onSubmit={handleSubmit}>
       <input
-        onChange={() => dispatch({ type: "addName" })}
+        onChange={(e) => dispatch({ type: "addName", payload: e.target.value })}
         className="border border-purple-500 p-2 m-5"
         type="text"
         name="name"
@@ -30,6 +48,7 @@ const UserInfoWithUseReducer = () => {
         placeholder="Name"
       />
       <input
+        onChange={(e) => dispatch({ type: "addAge", payload: e.target.value })}
         className="border border-purple-500 p-2 m-5"
         type="number"
         name="age"
@@ -37,6 +56,7 @@ const UserInfoWithUseReducer = () => {
         placeholder="Age"
       />
       <input
+        onBlur={(e) => dispatch({ type: "addHobby", payload: e.target.value })}
         className="border border-purple-500 p-2 m-5"
         type="text"
         name="hobby"
