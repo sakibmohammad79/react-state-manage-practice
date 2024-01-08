ac49d5e8828622fb208dcc87efdf7ea4d62babed
sakibmohammad79 <sakibmohammad7679@gmail.com>
2024-01-08 21:40:44 +0600

    handle Global state

--- src/components/Todo/TodoForm.tsx
+++ src/components/Todo/TodoForm.tsx
@@ -1,13 +1,32 @@
-import { useContext } from "react";
+import { ChangeEvent, useContext, useState } from "react";
 import { todoContext } from "../context/TodoProvider";
 
 const TodoForm = () => {
-  const { todoTitle } = useContext(todoContext);
-  console.log(todoTitle);
+  const { state, dispatch } = useContext(todoContext);
+  console.log(state);
+  const [task, setTask] = useState("");
+  const handleSubmit = (e: ChangeEvent) => {
+    e.preventDefault();
+    const todo = {
+      id: Math.random().toString(36).substring(2, 7),
+      title: task,
+      isCompleted: false,
+    };
+    dispatch({ type: "addTodo", payload: todo });
+  };
   return (
-    <div>
-      <h2>This is a todo form conponent</h2>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="todo">Task</label>
+      <input
+        className="border border-red-600"
+        onBlur={(e) => setTask(e.target.value)}
+        type="text"
+        name="todo"
+        id="todo"
+        placeholder="Todo"
+      />
+      <input type="submit" value="Submit" />
+    </form>
   );
 };
 
--- src/components/context/TodoProvider.tsx
+++ src/components/context/TodoProvider.tsx
@@ -1,11 +1,42 @@
-import { createContext } from "react";
+import { ReactNode, createContext, useReducer } from "react";
 
-export const todoContext = createContext(undefined);
-const TodoProvider = ({ children }) => {
-  const todo = {
-    todoTitle: "Programing Hero",
+export const todoContext = createContext<
+  { state: TTodo[]; dispatch: React.Dispatch<TAction> } | undefined
+>(undefined);
+
+type TTodo = {
+  id: string;
+  title: string;
+  isCompleted: boolean;
+};
+
+type TTodoProviderProps = {
+  children: ReactNode;
+};
+
+type TAction = {
+  type: string;
+  payload: TTodo;
+};
+
+const initialState: TTodo[] = [];
+
+const reducer = (currentState: TTodo[], action: TAction) => {
+  switch (action.type) {
+    case "addTodo":
+      return [...currentState, action.payload];
+    default:
+      return currentState;
+  }
+};
+
+const TodoProvider = ({ children }: TTodoProviderProps) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const value = {
+    state,
+    dispatch,
   };
-  return <todoContext.Provider value={todo}>{children}</todoContext.Provider>;
+  return <todoContext.Provider value={value}>{children}</todoContext.Provider>;
 };
 
 export default TodoProvider;
